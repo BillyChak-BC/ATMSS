bd19ec44a216ac284276b2d75f9a6ed86b08752c
Billy <die777990755@gmail.com>
2021-03-27 14:09:41 +0800

    Update CardReaderEmulatorController.java

--- src/ATMSS/CardReaderHandler/Emulator/CardReaderEmulatorController.java
+++ src/ATMSS/CardReaderHandler/Emulator/CardReaderEmulatorController.java
@@ -18,129 +18,132 @@ import javafx.scene.layout.StackPane;
 //======================================================================
 // CardReaderEmulatorController
 public class CardReaderEmulatorController {
-	private String id;
-	private AppKickstarter appKickstarter;
-	private Logger log;
-	private CardReaderEmulator cardReaderEmulator;
-	private MBox cardReaderMBox;
-	public TextField cardNumField1;
-	public TextField cardNumField2;
-	public TextField cardStatusField;
-	public TextArea cardReaderTextArea;
-	public Button insertBtn;
-	public Button removeBtn;
+    private String id;
+    private AppKickstarter appKickstarter;
+    private Logger log;
+    private CardReaderEmulator cardReaderEmulator;
+    private MBox cardReaderMBox;
+    public TextField cardNumField1;
+    public TextField cardNumField2;
+    public TextField cardStatusField;
+    public TextArea cardReaderTextArea;
+    public Button insertBtn;
+    public Button removeBtn;
 
-	//------------------------------------------------------------
-	// initialize
-	public void initialize(String id, AppKickstarter appKickstarter, Logger log, CardReaderEmulator cardReaderEmulator) {
-		this.id = id;
-		this.appKickstarter = appKickstarter;
-		this.log = log;
-		this.cardReaderEmulator = cardReaderEmulator;
-		this.cardReaderMBox = appKickstarter.getThread("CardReaderHandler").getMBox();
-	} // initialize
-
-
-	//------------------------------------------------------------
-	// textFieldPressed
-	public void textFieldPress1(KeyEvent keyEvent) {
-		// Auto Focus
-		TextField tf = (TextField) keyEvent.getSource();
-		if (tf.getLength() == 4) {
-			cardNumField2.requestFocus();
-		}
-	}
-
-	public void textFieldPress2(KeyEvent keyEvent) {
-		// Limit 4
-		TextField tf = (TextField) keyEvent.getSource();
-		if (tf.getLength() == 4) {
-			//cardReaderTextArea.requestFocus();
-			insertBtn.requestFocus();
-		}
-		log.info(tf.getText());
-	}
+    //------------------------------------------------------------
+    // initialize
+    public void initialize(String id, AppKickstarter appKickstarter, Logger log, CardReaderEmulator cardReaderEmulator) {
+        this.id = id;
+        this.appKickstarter = appKickstarter;
+        this.log = log;
+        this.cardReaderEmulator = cardReaderEmulator;
+        this.cardReaderMBox = appKickstarter.getThread("CardReaderHandler").getMBox();
+    } // initialize
 
 
-	//------------------------------------------------------------
-	// buttonPressed
-	public void buttonPressed(ActionEvent actionEvent) {
-		Button btn = (Button) actionEvent.getSource();
+    //------------------------------------------------------------
+    // textFieldPressed
+    public void textFieldPress1(KeyEvent keyEvent) {
+        // Auto Focus
+        TextField tf = (TextField) keyEvent.getSource();
+        if (tf.getLength() == 4 && cardNumField2.getLength() != 4) {
+            cardNumField2.requestFocus();
+        }else if (tf.getLength() == 4 && cardNumField2.getLength() == 4){
+            insertBtn.requestFocus();
+        }
+    }
 
-		switch (btn.getText()) {
-			case "Card 1":
-				String cardNum1 = appKickstarter.getProperty("CardReader.Card1");
-				cardNumField1.setText(cardNum1.substring(0,4));
-				cardNumField2.setText(cardNum1.substring(5,9));
-				break;
+    public void textFieldPress2(KeyEvent keyEvent) {
+        // Limit 4
+        TextField tf = (TextField) keyEvent.getSource();
+        if (tf.getLength() == 4 && cardNumField1.getLength() == 4) {
+            insertBtn.requestFocus();
+        }else if (tf.getLength() == 4 && cardNumField1.getLength() != 4){
+            cardNumField1.requestFocus();
+        }
+        log.info(tf.getText());
+    }
 
-			case "Card 2":
-				String cardNum2 = appKickstarter.getProperty("CardReader.Card2");
-				cardNumField1.setText(cardNum2.substring(0,4));
-				cardNumField2.setText(cardNum2.substring(5,9));
-				break;
 
-			case "Card 3":
-				String cardNum3 = appKickstarter.getProperty("CardReader.Card3");
-				cardNumField1.setText(cardNum3.substring(0,4));
-				cardNumField2.setText(cardNum3.substring(5,9));
-				break;
+    //------------------------------------------------------------
+    // buttonPressed
+    public void buttonPressed(ActionEvent actionEvent) {
+        Button btn = (Button) actionEvent.getSource();
 
-			case "Reset":
-				cardNumField1.setText("");
-				cardNumField2.setText("");
-				break;
+        switch (btn.getText()) {
+            case "Card 1":
+                String cardNum1 = appKickstarter.getProperty("CardReader.Card1");
+                cardNumField1.setText(cardNum1.substring(0,4));
+                cardNumField2.setText(cardNum1.substring(5,9));
+                break;
 
-			case "Insert Card":
-				// if (cardNumField.getText().length() != 0) {
-				String cardText;
-				cardText = cardNumField1.getText() + "-" + cardNumField2.getText();
-				cardReaderMBox.send(new Msg(id, cardReaderMBox, Msg.Type.CR_CardInserted, cardText));
-				cardReaderTextArea.appendText("Sending " + cardText+"\n");
+            case "Card 2":
+                String cardNum2 = appKickstarter.getProperty("CardReader.Card2");
+                cardNumField1.setText(cardNum2.substring(0,4));
+                cardNumField2.setText(cardNum2.substring(5,9));
+                break;
+
+            case "Card 3":
+                String cardNum3 = appKickstarter.getProperty("CardReader.Card3");
+                cardNumField1.setText(cardNum3.substring(0,4));
+                cardNumField2.setText(cardNum3.substring(5,9));
+                break;
+
+            case "Reset":
+                cardNumField1.setText("");
+                cardNumField2.setText("");
+                break;
+
+            case "Insert Card":
+                // if (cardNumField.getText().length() != 0) {
+                String cardText;
+                cardText = cardNumField1.getText() + "-" + cardNumField2.getText();
+                cardReaderMBox.send(new Msg(id, cardReaderMBox, Msg.Type.CR_CardInserted, cardText));
+                cardReaderTextArea.appendText("Sending " + cardText+"\n");
 //		    cardStatusField.setText("Card Inserted");
-				// }
-				break;
+                // }
+                break;
 
-			case "Remove Card":
-				if (cardStatusField.getText().compareTo("Card Ejected") == 0) {
-					cardText = cardNumField1.getText() + "-" + cardNumField2.getText();
+            case "Remove Card":
+                if (cardStatusField.getText().compareTo("Card Ejected") == 0) {
+                    cardText = cardNumField1.getText() + "-" + cardNumField2.getText();
 
-					cardReaderTextArea.appendText("Removing card\n");
-					cardReaderMBox.send(new Msg(id, cardReaderMBox, Msg.Type.CR_CardRemoved, cardText));
-				}
-				break;
-
-
-			default:
-				log.warning(id + ": unknown button: [" + btn.getText() + "]");
-				break;
-		}
-	} // buttonPressed
+                    cardReaderTextArea.appendText("Removing card\n");
+                    cardReaderMBox.send(new Msg(id, cardReaderMBox, Msg.Type.CR_CardRemoved, cardText));
+                }
+                break;
 
 
-	//------------------------------------------------------------
-	// updateCardStatus
-	public void updateCardStatus(String status) {
-		cardStatusField.setText(status);
-		if (status.compareTo("Card Inserted") == 0) {
-			insertBtn.setDisable(true);
-		} else if (status.compareTo("Card Ejected") == 0) {
-			removeBtn.setDisable(false);
-		} else if (status.compareTo("Card Reader Empty") == 0) {
-			insertBtn.setDisable(false);
-			removeBtn.setDisable(true);
-		}
-	} // updateCardStatus
-
-	public void clearCardNum(){
-		cardNumField1.setText("");
-		cardNumField2.setText("");
-	}
+            default:
+                log.warning(id + ": unknown button: [" + btn.getText() + "]");
+                break;
+        }
+    } // buttonPressed
 
 
-	//------------------------------------------------------------
-	// appendTextArea
-	public void appendTextArea(String status) {
-		cardReaderTextArea.appendText(status+"\n");
-	} // appendTextArea
+    //------------------------------------------------------------
+    // updateCardStatus
+    public void updateCardStatus(String status) {
+        cardStatusField.setText(status);
+        if (status.compareTo("Card Inserted") == 0) {
+            insertBtn.setDisable(true);
+        } else if (status.compareTo("Card Ejected") == 0) {
+            removeBtn.setDisable(false);
+        } else if (status.compareTo("Card Reader Empty") == 0) {
+            insertBtn.setDisable(false);
+            removeBtn.setDisable(true);
+        }
+    } // updateCardStatus
+
+    public void clearCardNum(){
+        cardNumField1.setText("");
+        cardNumField2.setText("");
+    }
+
+
+    //------------------------------------------------------------
+    // appendTextArea
+    public void appendTextArea(String status) {
+        cardReaderTextArea.appendText(status+"\n");
+    } // appendTextArea
 } // CardReaderEmulatorController
